423f202732d5b0017d455af0e722d6730c5245fd
atiger7391 <atiger7391@gmail.com>
2020-04-17 20:06:26 +0900

    Test calling api.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Amplify, { API } from 'aws-amplify';
+import awsconfig from './aws-exports';
+
+Amplify.configure(awsconfig);
+
+console.log(1)
+
+let apiName = 'sandboxVWAAPI';
+let path = '/events'; 
+let myInit = { 
+    headers: {}, 
+    response: true,
+    queryStringParameters: {
+        name: 'param'
+    }
+}
+API.get(apiName, path, myInit).then(response => {
+    console.log(response);
+}).catch(error => {
+    console.log(error.response)
+});
 
 Vue.config.productionTip = false
 
