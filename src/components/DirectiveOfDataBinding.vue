6f04de69535b932638cd193b1fbe3f90cbc88eb5
atiger7391 <atiger7391@gmail.com>
2020-04-27 03:29:00 +0900

    Add directive of data-binding.

--- src/components/DirectiveOfDataBinding.vue
+++ src/components/DirectiveOfDataBinding.vue
@@ -3,8 +3,34 @@
     <h2>Study Vue directive(Data binding) </h2>
 
     <div class="block">
-      
+      <v-form
+        ref="form"
+        lazy-validation
+      >
+        <v-text-field
+          name="email"
+          label="input email"
+          id="email"
+          :rules="emailRules"
+        ></v-text-field>
+        <input type="text" id="email2" v-bind="attrs" style="border: 1px solid #ccc" />
+        <v-btn color="info" @click="validateEmail">submit</v-btn>
+      </v-form>
+    </div>
 
+
+    <div class="block">
+      <select  v-model="picSizeDiv">
+        <option value="height">高さ</option>
+        <option value="width">幅</option>
+      </select>：
+      <input type="text" v-model="picSize" /><br>
+      <img src="https://wings.msn.to/image/wings.jpg" :[picSizeDiv]="picSize">
+    </div>
+
+
+    <div class="block">
+      <p v-html="innerHTML"></p>
     </div>
 
   </div>
@@ -14,6 +40,18 @@
 <script>
 export default {
   data: () => ({
+    attrs: {
+      size     :20,
+      maxlength:10,
+      required :true
+    },
+    emailRules: [
+      v => !!v || 'E-mail is required',
+      v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+    ],
+    picSizeDiv: "width",
+    picSize: 100,
+    innerHTML:"<h3>hogehoge</h3><br><ul><li>aaaa</li><li>aaaa</li></ul>",
   })
   ,watch: {
   }
@@ -22,6 +60,9 @@ export default {
   ,computed: {
   }
   ,methods: {
+    validateEmail() {
+      this.$refs.form.validate();
+    }
   }
 }
 </script>
