5255a2a869b110da2a592313744e087db5e4a1e3
atiger7391 <atiger7391@gmail.com>
2020-05-12 23:57:33 +0900

    Study async store.

--- src/store/index.js
+++ src/store/index.js
@@ -3,11 +3,43 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+export const ADD_BOOK = 'addBook';
+
 export default new Vuex.Store({
   strict: true,
   state: {
     count: 0,
     count2: 0,
+    books: [
+      {
+        isbn: "11111",
+        title: "title1",
+        price: 1000,
+      },
+      {
+        isbn: "22222",
+        title: "title2",
+        price: 2000,
+      },
+      {
+        isbn: "33333",
+        title: "title3",
+        price: 3000,
+      },
+    ],
+  },
+  getters: {
+    getBooksCount(state) {
+      return state.books.length;
+    },
+    getBooksByPrice(state) {
+      return price => {
+        return state.books.filter(book => book.price < price)
+      }
+    },
+    getBookCountOver3000(state, getters) {
+      return getters.getBooksByPrice(3000).length
+    }
   },
   mutations: {
     minus(state) {
@@ -17,9 +49,17 @@ export default new Vuex.Store({
     plus(state) {
       state.count++;
       state.count2++;
+    },
+    [ADD_BOOK](state, payload) {
+      state.books.push(payload.book);
     }
   },
   actions: {
+    addAsync(context, payload) {
+      setTimeout(() => {
+        context.commit(ADD_BOOK, payload)
+      }, 5000)
+    }
   },
   modules: {
   }
--- src/views/StudyStore.vue
+++ src/views/StudyStore.vue
@@ -1,28 +1,92 @@
 <template> 
   <div>
     <v-container grid-list-xs>
-      <v-btn rounded dark color="blue" value="-" @click="plus">plus</v-btn>
-      <v-btn rounded dark color="red" value="-" @click="minus">minus</v-btn>
-      <br>
-      computed: {{count}}
-      <br>
-      mapState: {{count2}}
+      <v-card >
+        <v-card-title primary-title>
+          Simple mutation sample
+        </v-card-title>
+        <v-card-actions>
+          <v-btn rounded dark color="blue" value="-" @click="plus">plus</v-btn>
+          <v-btn rounded dark color="red" value="-" @click="minus">minus</v-btn>
+        </v-card-actions>
+        <v-card-text>
+          computed: {{count}}
+          <br>
+          mapState: {{count2}}
+        </v-card-text>
+      </v-card>
+      <v-card>
+        <v-card-title primary-title>
+          Use Getters sample.
+        </v-card-title>
+        <v-card-actions>
+          <v-text-field hide-details="false"  class="mt-0"
+            name="price"
+            label="Search condition price"
+            v-model="searchConditionPrice"
+          ></v-text-field>
+        </v-card-actions>
+        <v-card-text>
+          Books count is ＜ {{getBooksCount}} ＞
+          <br>
+          <ul v-for="b in getBooksByPrice(this.searchConditionPrice)" :key="b.isbn">
+            <li>{{b.title}} (\{{b.price}}) ISBN : {{b.isbn}}</li>
+          </ul>
+        </v-card-text>
+      </v-card>
+      <v-card>
+        <v-card-title primary-title>
+          Add book with mutation.
+        </v-card-title>
+        <v-row>
+          <v-col>
+            <v-card-actions>
+              <v-Form @submit.prevent="addBook" >
+                <v-text-field hide-details="false"  class="mt-0" name="isbn" label="ISBN NO" v-model="book.isbn" id="isbn"></v-text-field>
+                <v-text-field hide-details="false"  class="mt-0" name="title" label="TITLE" v-model="book.title" id="title"></v-text-field>
+                <v-text-field                       class="mt-0" name="price" label="PRICE" v-model.number="book.price" id="price" :rules="priceRules"></v-text-field>
+                <v-btn type="submit" color="info" dark>Sync submit!</v-btn>
+                <v-btn type="submit" @click="asyncAddBook" color="info" dark>Async submit!</v-btn>
+              </v-Form>
+            </v-card-actions>
+          </v-col>
+          <v-col>
+            <v-list max-height="25px" dense>
+              <v-list-item v-for="b in getBooksByPrice(1000000)" :key="b.isbn">
+                <v-list-item-content>
+                  {{b.title}} (\{{b.price}}) ISBN : {{b.isbn}}
+                </v-list-item-content>
+              </v-list-item>
+            </v-list>
+          </v-col>
+        </v-row>
+      </v-card>
     </v-container>
   </div>
 </template> 
 
 <script> 
 
-import {mapState} from 'vuex';
+import {mapState, mapGetters} from 'vuex';
 
 export default { 
   data: ()=> ({
+    searchConditionPrice: 0,
+    book: {
+      isbn  : '',
+      title : '',
+      price : null,
+    }
   }),
   computed: {
     count() {
       return this.$store.state.count;
     },
     ...mapState(['count2']),
+    ...mapGetters(['getBooksCount', 'getBooksByPrice', 'getBookCountOver3000']),
+    priceRules: function() {
+      return [v => (v >= 0 && v <= 10000) || "Input number between 0 and 10000 !"]
+    }
   },
   methods: {
     plus() {
@@ -31,6 +95,18 @@ export default {
     minus() {
       this.$store.commit('minus');
     },
+    addBook() {
+      this.$store.commit('addBook', {book:
+        {
+          isbn: this.book.isbn,
+          title: this.book.title,
+          price: this.book.price,
+        }
+      })
+    },
+    asyncAddBook() {
+      this.$store.dispatch('addAsync', {book: this.book})
+    }
   },
 } 
 </script> 
